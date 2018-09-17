12cdda673c6e356b75d8801f4bf1c50b02001b72
Gabriel Crevelario <gabrielcrevelario2@gmail.com>
2018-09-17 01:41:48 -0300

    testando a primeira classe de dominio

--- src/main/java/com/crevelario/resources/CategoriaResources.java
+++ src/main/java/com/crevelario/resources/CategoriaResources.java
@@ -1,15 +1,25 @@
 package com.crevelario.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.crevelario.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest funcionando";	
+	public List <Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Terror");
+		Categoria cat2 = new Categoria(2,"Comedia");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;	
 	}
 	
 }
