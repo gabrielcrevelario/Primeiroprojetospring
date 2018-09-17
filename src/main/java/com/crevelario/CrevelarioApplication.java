2e63af23623ed624d8a914b9e4862e1789ce8950
Gabriel Crevelario <gabrielcrevelario2@gmail.com>
2018-09-17 01:06:03 -0300

    fiu

---
+++ src/main/java/com/crevelario/CrevelarioApplication.java
@@ -0,0 +1,12 @@
+package com.crevelario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrevelarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrevelarioApplication.class, args);
+	}
+}
